a24da3fb4de21c11a0f1cee7327ed3a2d2799f3a
Andri Saputra <andri.saputra25021@gmail.com>
2026-04-23 08:27:01 +0700

    feat: setup axios, services/api, and custom hooks for courses

---
+++ mission-adv-fe1/src/hooks/useCourses.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import {
+  getCourses,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} from "../services/api/cardsAPI";
+
+const useCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // get all cards
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const data = await getCourses();
+      setCourses(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  //tambah card
+  const createCourse = async (newCourse) => {
+    try {
+      const data = await addCourse(newCourse);
+      setCourses((prev) => [...prev, data]);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  //update card
+  const editCourse = async (id, updateCourse) => {
+    try {
+      const data = await updateCourse(id, updateCourse);
+      setCourses((prev) =>
+        prev.map((course) => (course.id === id ? data : course)),
+      );
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  //delete card
+  const removeCourse = async (id) => {
+    try {
+      await deleteCourse(id);
+      setCourses((prev) => prev.filter((course) => course.id !== id));
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  return {
+    courses,
+    loading,
+    error,
+    fetchCourses,
+    createCourse,
+    editCourse,
+    removeCourse,
+  };
+};
+
+export default useCourses;
